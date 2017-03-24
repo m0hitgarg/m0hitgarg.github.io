474480d66496fae91592e8a71768507d1279411c
Mohit Garg <gargmohit@live.in>
2017-03-24 20:58:58 +0530

    list items added

--- todolist/js/app.js
+++ todolist/js/app.js
@@ -1,1 +1,8 @@
-console.log(document.getElementById('add'));
+
+// User clicked on the add button
+document.getElementById('add').addEventListener('click' , function(){
+  let value = document.getElementById('item').value;
+  if(value) {
+    
+  }
+});
