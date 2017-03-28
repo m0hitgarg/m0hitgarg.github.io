17c7e6cf7d288e2991e6feec78a84b2c11a2bbb4
Mohit Garg <gargmohit@live.in>
2017-03-28 06:37:55 +0530

    close and delete function added

--- issuetracker/js/main.js
+++ issuetracker/js/main.js
@@ -17,20 +17,63 @@ function saveIssue(e) {
     status:issueStatus
   }
 
-var issuesList = document.getElementById('issueList');
-var el = document.createElement('div')
+  if(localStorage.getItem('issues') === null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues' , JSON.stringify(issues));
+  } else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues' , JSON.stringify(issues));
+  }
 
-console.log("Hello from Issue");
-  el.innerHTML =   '<div class="well">'+
-                            '<h6>Issue ID: ' + issueID + '</h6>'+
-                            '<p><span class="label label-info">' + issueStatus + '</span></p>'+
-                            '<h3>' + issueDescription + '</h3>'+
-                            '<p><span class="glyphicon glyphicon-time"></span> ' + issueSeverity + '</p>'+
-                            '<p><span class="glyphicon glyphicon-user"></span> ' + issueAssigned + '</p>'+
-                            '<a href="#" id="close"  class="btn btn-warning">Close</a> '+
-                            '<a href="#" id="delete" class="btn btn-danger">Delete</a>'+
-                            '</div>';
-issuesList.appendChild(el);
-e.preventDefault();
 submit.reset();
+fetchIssues();
+e.preventDefault();
+}
+
+function fetchIssues() {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  var issuesList = document.getElementById('issueList');
+  issuesList.innerHTML = '';
+
+ for (var i = 0; i < issues.length; i++) {
+   var id = issues[i].id;
+   var desc = issues[i].description;
+   var severity = issues[i].severity;
+   var assignedTo = issues[i].assignedTo;
+   var status = issues[i].status;
+
+   issuesList.innerHTML +=   '<div class="well">'+
+                             '<h6>Issue ID: ' + id + '</h6>'+
+                             '<p><span class="label label-info">' + status + '</span></p>'+
+                             '<h3>' + desc + '</h3>'+
+                             '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
+                             '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                             '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
+                             '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                             '</div>';
+ }
+}
+
+function setStatusClosed(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  for (var i = 0; i < issues.length; i++) {
+    if(issues[i].id === id) {
+      issues[i].status = 'Closed';
+    }
+  }
+  localStorage.setItem('issues' , JSON.stringify(issues));
+  fetchIssues();
+}
+
+function deleteIssue(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  for (var i = 0; i < issues.length; i++) {
+    if(issues[i].id === id) {
+      issues.splice(i,1);
+    }
+  }
+  localStorage.setItem('issues' , JSON.stringify(issues));
+  fetchIssues();
 }
